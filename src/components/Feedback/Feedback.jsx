aee71b8f264f6f3395bd6b744c24f54d27a28844
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-05-28 19:54:00 +0200

    Fix path

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,3 +1,5 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {} from './Feedback.styled';
+
+const Feedback {}
