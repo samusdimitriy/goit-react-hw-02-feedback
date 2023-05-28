db8ae7039ccacffce0a89f8e94f2c8dee76876f2
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-05-28 19:14:31 +0200

    Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Feedback from './Feedback';
+import Statistics from './Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positiveFeedback = Math.round((good / total) * 100);
+
+    return (
+      <>
+        <Feedback></Feedback>
+        <Statistics></Statistics>
+      </>
+    );
+  }
+}
+
+export default App;
